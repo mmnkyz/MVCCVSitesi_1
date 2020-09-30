f1f8a9f091ce7f3b435fe52260b1c731fb8f7fac
Mümin Akyüz <mumin.akyuz@AKMERCAN.COM.TR>
2020-09-30 09:23:15 +0300

    Add project files.

---
+++ MVCCVSitesi_1/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCVSitesi_1.Models.Entities;
+
+namespace MVCCVSitesi_1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        DBMVCCVEntities db = new DBMVCCVEntities();
+        public ActionResult Index()
+        {
+            var bilgiler = db.TBLHAKKIMDA.ToList();
+            return View(bilgiler);
+        }
+    }
+}
